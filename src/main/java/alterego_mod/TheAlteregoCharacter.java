3d01dbb29aea9127f6fb3337593d7ace0e1eee68
Ryan Turner <ryman9109thedoctor@gmail.com>
2020-07-14 14:34:22 -0400

    Added Skirt.

--- src/main/java/alterego_mod/TheAlteregoCharacter.java
+++ src/main/java/alterego_mod/TheAlteregoCharacter.java
@@ -165,7 +165,7 @@ public class TheAlteregoCharacter extends CustomPlayer {
         retVal.add("alterego_mod:Defend");
         retVal.add("alterego_mod:BigMeatyClaws");
 
-        retVal.add("alterego_mod:Cushion");
+        retVal.add("alterego_mod:Skirt");
         return retVal;
     }
 
---
+++ src/main/java/cards/PassionlipSkirt.java
@@ -0,0 +1,62 @@
+package cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.utility.ScryAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.DexterityPower;
+import com.megacrit.cardcrawl.powers.FrailPower;
+import com.megacrit.cardcrawl.powers.VulnerablePower;
+
+import basemod.abstracts.CustomCard;
+
+import static alterego_mod.AbstractCardEnum.Passionlip_Purple;
+
+public class PassionlipSkirt
+        extends CustomCard {
+    public static final String ID = "alterego_mod:Skirt";
+    private static CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    // Get object containing the strings that are displayed in the game.
+    public static final String NAME = cardStrings.NAME;
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+    public static final String IMG_PATH = "images/cards/Skirt.png";
+    private static final int COST = 1;
+    private static final int UPGRADE_PLUS_DMG = 4;
+
+    public PassionlipSkirt() {
+        super(ID, NAME, IMG_PATH, COST, DESCRIPTION,
+                CardType.ATTACK, Passionlip_Purple,
+                CardRarity.COMMON, CardTarget.ENEMY);
+        this.baseDamage = 4;
+        this.baseMagicNumber = 2;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        AbstractDungeon.actionManager.addToBottom(new com.megacrit.cardcrawl.actions.common.DamageAction(m,
+                new DamageInfo(p, this.damage, this.damageTypeForTurn),
+                AbstractGameAction.AttackEffect.SLASH_DIAGONAL));
+
+        this.addToBot(new ScryAction(this.magicNumber));
+    }
+
+    @Override
+    public AbstractCard makeCopy() {
+        return new PassionlipSkirt();
+    }
+
+    @Override
+    public void upgrade() {
+        if (!this.upgraded) {
+            this.upgradeName();
+            this.upgradeDamage(UPGRADE_PLUS_DMG);
+            this.upgradeMagicNumber(1);
+        }
+    }
+}
