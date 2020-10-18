60fa51d771d29a13e7921b878065af711fc91501
Ryan Turner <ryman9109thedoctor@gmail.com>
2020-10-18 17:57:58 -0400

    Add animation for Brynhildr stance.

--- src/main/java/stances/PassionlipBrynhildr.java
+++ src/main/java/stances/PassionlipBrynhildr.java
@@ -1,6 +1,8 @@
 package stances;
 
 import alterego_mod.AlteregoMod;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.MathUtils;
 import com.megacrit.cardcrawl.actions.common.DrawCardAction;
 import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
 import com.megacrit.cardcrawl.actions.common.HealAction;
@@ -16,6 +18,10 @@ import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.StanceStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.stances.AbstractStance;
+import com.megacrit.cardcrawl.vfx.stance.CalmParticleEffect;
+import com.megacrit.cardcrawl.vfx.stance.StanceAuraEffect;
+import vfx.AlteregoBrynhildrParticleEffect;
+import vfx.AlteregoStanceAuraEffect;
 
 public class PassionlipBrynhildr extends AbstractStance {
     public static final String STANCE_ID = "alterego_mod:Brynhildr";
@@ -39,5 +45,22 @@ public class PassionlipBrynhildr extends AbstractStance {
 
     public void onMonsterDeath(AbstractMonster m) {
         AbstractDungeon.actionManager.addToBottom(new HealAction(AbstractDungeon.player, AbstractDungeon.player, 7));
+    }
+
+    public void updateAnimation() {
+        if (!Settings.DISABLE_EFFECTS) {
+            this.particleTimer -= Gdx.graphics.getDeltaTime();
+            if (this.particleTimer < 0.0F) {
+                this.particleTimer = 0.04F;
+                AbstractDungeon.effectsQueue.add(new AlteregoBrynhildrParticleEffect());
+            }
+        }
+
+        this.particleTimer2 -= Gdx.graphics.getDeltaTime();
+        if (this.particleTimer2 < 0.0F) {
+            this.particleTimer2 = MathUtils.random(0.45F, 0.55F);
+            AbstractDungeon.effectsQueue.add(new AlteregoStanceAuraEffect(ID));
+        }
+
     }
 }
